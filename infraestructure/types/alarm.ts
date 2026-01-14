392063031cc1de6cdd15dd6342635d88c92dd7c6
elgabrielcisneros <tilordqwertydev3@gmail.com>
2026-01-13 21:12:40 -0400

    add types & status

--- infraestructure/types/alarm.ts
+++ infraestructure/types/alarm.ts
@@ -3,17 +3,35 @@ export type AlarmStore = {
   addAlarm: (alarm: Alarm) => void;
   removeAlarm: (id: string) => void;
   updateAlarm: (alarm: Alarm) => void;
+  clearAlarms: () => void;
 };
 
 export type Alarm = {
   id: string;
   title: string;
-  time: string;
-  isRepeating: boolean;
-  challenge: ChallengeType;
-  days: string[];
-  volume: number;
-  isMuted: boolean;
+  time: Date;
+  repeating: boolean;
+  repeatingPattern: RepeatingPattern;
+  challenge: Challenge;
+  day: Day;
+  volume: Volume;
+  status: AlarmStatus;
 };
 
+export type AlarmStatus = "enabled" | "disabled";
+
+export type Day = "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
+
+export type RepeatingPattern = "daily" | "weekly";
+
 export type ChallengeType = "math" | "qr" | "walk";
+
+export type Volume = {
+  value: number;
+  muted: boolean;
+};
+
+export type Challenge = {
+  type: ChallengeType;
+  status: "not_started" | "started" | "completed";
+};
