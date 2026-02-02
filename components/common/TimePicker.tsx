47dfa81f7e66014ba6d60656aadccb885482a601
elgabrielcisneros <tilordqwertydev3@gmail.com>
2026-02-02 15:53:47 -0400

    add picker styles  🎨
    - center picker component
    - change font size on android files

--- components/common/TimePicker.tsx
+++ components/common/TimePicker.tsx
@@ -1,13 +1,30 @@
 import { View } from "@/components/Themed";
 import { useState } from "react";
+import { StyleSheet } from "react-native";
 import DatePicker from "react-native-date-picker";
+import "../../styles/global.css";
 
 export default function TimePicker() {
   const [time, setTime] = useState(new Date());
 
   return (
     <View>
-      <DatePicker date={time} onDateChange={setTime} />
+      <View className="flex items-center justify-center mt-10">
+        <DatePicker
+          date={time}
+          onDateChange={setTime}
+          mode="time"
+          style={styles.datePicker}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  datePicker: {
+    width: 400,
+    height: 200,
+    color: "#64a7ffff",
+  },
+});
