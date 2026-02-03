63071e1e814d4c9a539ea72a27c57e7a4a74c13d
elgabrielcisneros <tilordqwertydev3@gmail.com>
2026-02-03 17:17:43 -0400

    improve styles

--- components/common/NameRepetitionCard.tsx
+++ components/common/NameRepetitionCard.tsx
@@ -2,13 +2,11 @@ import { Text, View } from "@/components/Themed";
 import Ionicons from "@react-native-vector-icons/ionicons";
 import React from "react";
 import { Pressable, StyleSheet, TextInput } from "react-native";
+import "../../styles/global.css";
 
 export default function NameRepetitionCard() {
   return (
-    <View
-      style={styles.card}
-      className="flex items-center justify-center mt-10 p-3"
-    >
+    <View style={styles.card} className="flex items-center justify-center p-3">
       <View
         style={styles.labelInputContainer}
         className="flex-row items-center w-full"
@@ -22,8 +20,9 @@ export default function NameRepetitionCard() {
           className="text-white"
         />
       </View>
+
       <View
-        className="flex-row items-center justify-between w-full"
+        className="flex-row items-center justify-between w-full mt-6 mb-4"
         style={styles.labelInputContainer}
       >
         <Text className="mr-4" style={styles.label}>
@@ -31,7 +30,7 @@ export default function NameRepetitionCard() {
         </Text>
 
         <Pressable>
-          <Text className="text-white text-2xl">
+          <Text className="text-white text-2xl" style={styles.repeatText}>
             Never
             <Ionicons
               name="chevron-forward"
@@ -51,7 +50,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#1E293B",
     borderColor: "#94A3B8",
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 30,
   },
   label: {
     fontFamily: "Manrope",
@@ -67,5 +66,8 @@ const styles = StyleSheet.create({
   },
   labelInputContainer: {
     backgroundColor: "transparent",
+  },
+  repeatText: {
+    fontFamily: "Manrope",
   },
 });
